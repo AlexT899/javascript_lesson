615318db3ea99901d9c1c1ab0faeca3c19706318
Aleksejs Tarvids <lexa500@yahoo.ie>
2026-03-25 22:15:22 +0000

    Add files via upload
    add homework 9

---
+++ DOM/js/script.js
@@ -0,0 +1,33 @@
+const title = document.getElementsByTagName("h1");
+
+const calculateButton = document.getElementsByClassName("handler_btn");
+
+const resetButton = document.getElementsByClassName("handler_btn");
+
+const addButton = document.querySelector(".screen-btn");
+
+const otherItemsPercent = document.querySelectorAll(".other-items.percent");
+
+const otherItemsNumber = document.querySelectorAll(".other-items.number");
+
+const inputType = document.querySelector('.rollback  input[type="range"]');
+
+const span = document.querySelector(".rollback .range-value");
+
+const allInput = document.getElementsByClassName("total-input");
+const firstInput = allInput[0];
+for (let i = 0; i < allInput.length; i++) {
+  console.log(allInput[i]);
+}
+let screen = document.querySelectorAll(".screen");
+
+console.log(title[0]);
+console.log(calculateButton[0]);
+console.log(resetButton[1]);
+console.log(addButton);
+console.log(otherItemsPercent);
+console.log(otherItemsNumber);
+console.log(inputType);
+console.log(span);
+console.log(allInput);
+console.log(screen);
